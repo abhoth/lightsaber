0c66e550d676aba4202d582781c9d0a09bf9273a
Paul Zane <paul@zanesworld.net>
2016-03-04 11:20:26 -0500

    Added transitions

--- js/Battle.js
+++ js/Battle.js
@@ -12,7 +12,10 @@ function Battle(image, image2, trigger){
 			
 			x = randomize(30, 150);
 			y  = randomize(-30, -150);
-			
+			z = randomize(1,10);
+			a = randomize(1,10);
+			_this.image.css('transition','transform '+ z * .1 +'s');
+			_this.image2.css('transition','transform '+ a * .1 +'s');
 			_this.image.css('transform', 'rotate('+ x + 'deg)');
 			_this.image2.css('transform', 'rotate('+ y + 'deg)');
 			
